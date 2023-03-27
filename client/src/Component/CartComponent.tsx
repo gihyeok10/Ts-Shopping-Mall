5992cc45518ef2944a4061bf88395b1d76ac6ae2
kimgihyeok <gh9812301@naver.com>
2023-03-27 14:52:37 +0900

    장바구니 삭제기능

--- client/src/Component/CartComponent.tsx
+++ client/src/Component/CartComponent.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { ProductType } from "../Redux/action-types";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
+import { ProductInfo } from "../Redux/action-creators";
 type PropsTypes = {
   item: ProductType;
 };
 
 const CartComponent = (props: PropsTypes) => {
+  const dispatch: Dispatch = useDispatch();
+
   return (
     <div className="cartList">
       <img src={props.item.image_url} className="cart_Img" />
       <li>{props.item.name}</li>
       <li>{props.item.price}</li>
+      <button
+        onClick={() => {
+          dispatch(ProductInfo.product(props.item, "delete"));
+        }}
+      >
+        삭제
+      </button>
     </div>
   );
 };
