7875cc4125890fa7010eec926e2fd0087b87e69f
kimgihyeok <gh9812301@naver.com>
2023-03-27 14:50:04 +0900

    장바구니 조회

--- client/src/Component/CartComponent.tsx
+++ client/src/Component/CartComponent.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { ProductType } from "../Redux/action-types";
+type PropsTypes = {
+  item: ProductType;
+};
 
-const CartComponent = () => {
-  return <div>CartComponent</div>;
+const CartComponent = (props: PropsTypes) => {
+  return (
+    <div className="cartList">
+      <img src={props.item.image_url} className="cart_Img" />
+      <li>{props.item.name}</li>
+      <li>{props.item.price}</li>
+    </div>
+  );
 };
 
 export default CartComponent;
--- client/src/Pages/CartPage.tsx
+++ client/src/Pages/CartPage.tsx
@@ -5,23 +5,33 @@ import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "redux";
 import { ProductInfo } from "../Redux/action-creators";
 import { RootReducerType } from "../Redux/store";
-
+import CartComponent from "../Component/CartComponent";
 const CartPage = () => {
   const dispatch: Dispatch = useDispatch();
   const cartInfo = useSelector((state: RootReducerType) => state.allReducers);
-
+  const cartList = cartInfo.cart;
   const cartNull = () => {
     dispatch(ProductInfo.product(ProductNull, "all_delete"));
   };
   return (
     <div>
       <button onClick={cartNull}>장바구니 비우기</button>
+      <h1>장바구니 현황</h1>
+      {cartList &&
+        cartList.map((item, index) => {
+          return (
+            <div key={index}>
+              <CartComponent item={item} />
+            </div>
+          );
+        })}
     </div>
   );
 };
 
 export default CartPage;
 
+// 빈 프로덕트 오브젝트
 export const ProductNull: ProductType = {
   product_id: 0,
   price: "0",
